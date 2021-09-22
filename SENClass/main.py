d268d01f9ce58f28d809ba1670c95e1a411b96af
Felix Bachmann <fel.bachmann@gmail.com>
2021-09-22 17:13:23 +0200

    first initial commit

---
+++ SENClass/main.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+start_time = datetime.now()
+
+
+def main():
+    end_time = datetime.now()
+    print(f"\n end-time =", end_time - start_time, "Hr:min:sec \n")
+
+
+# main func
+if __name__ == '__main__':
+    main()
